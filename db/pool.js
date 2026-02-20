088defb3e562286f096d3f0b3c9696ca065f3f5f
madhavjari <213913746+madhavjari@users.noreply.github.com>
2026-02-20 18:53:26 +0530

    Changed pool for local/prod

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = isProduction
+  ? process.env.DB_URL
+  : process.env.DB_URL_LOCAL;
+
 module.exports = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false },
+  connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
